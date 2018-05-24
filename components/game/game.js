1c4bf3cc253c20c8508456fff82a1f1aca78cc49
Lawrence Sarpong <lawrenceeks@yahoo.com>
2018-05-24 12:58:09 +0100

    create handle letter select relates #10

--- components/game/game.js
+++ components/game/game.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Hint from '../hint/hint'; 
 import Chance from '../chance/chance'; 
 import Answer from '../answer/answer'; 
-import Letters from '../letter/letter';
+import Letters from '../letters/letters';
 import fetchGameData from '../../utils/fetchGameData/fetchGameData'
 import selectRandomCounty from "../../utils/selectRandomCounty/selectRandomCounty";
 
@@ -18,8 +18,13 @@ export default class Game extends React.Component {
         fetchGameData()
             .then(selectRandomCounty)
             .then(({ country, city }) => {
-                this.setState({ country, city })
+                this.setState({ country, city });
             });
+    }
+
+    handleSelected(event) {
+        console.log('letter clicked');
+        console.log(event.target);
     }
 
     render() {
@@ -29,7 +34,7 @@ export default class Game extends React.Component {
                 <Hint country={this.state.country}/>
                 <Chance chances={this.state.chances} />
                 <Answer city={this.state.city} />
-                <Letters />
+                <Letters selectHandler={this.handleSelected} />
             </React.Fragment>
         )
     }
